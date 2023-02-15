f56ec3548a4b5c458531d638107ffbc0bbd5af04
Paige C <77869251+pkcody@users.noreply.github.com>
2023-02-15 13:10:48 -0700

    flocking and build

---
+++ Steering Starter Project/Assets/Scripts/Behaviors/BlendSteering.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorAndWeight
+{
+    public SteeringBehavior behavior = null;
+    public float weight = 0f;
+}
+
+public class BlendSteering
+{
+    public BehaviorAndWeight[] behaviors;
+
+    float maxAcceleration = 5f;
+    float maxRotation = 7f;
+
+    public SteeringOutput getSteering()
+    {
+        SteeringOutput result = new SteeringOutput();
+
+        //get acc
+        foreach (BehaviorAndWeight b in behaviors)
+        {
+            SteeringOutput so = b.behavior.getSteering();
+            if (so != null)
+            {
+                result.angular += so.angular * b.weight;
+                result.linear += so.linear * b.weight;
+            }
+        }
+
+        result.linear = result.linear.normalized * maxAcceleration;
+        float angularAcceleration = Mathf.Abs(result.angular);
+        if (angularAcceleration > maxRotation)
+        {
+            result.angular /= angularAcceleration;
+            result.angular *= maxRotation;
+        }
+
+        return result;
+    }
+
+    
+}
---
+++ Steering Starter Project/Assets/Scripts/Flocker.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flocker : Kinematic
+{
+    public GameObject myCohereTarget;
+    BlendSteering blendSteering;
+    Kinematic[] kBirds;
+
+    void Start()
+    {
+        // Separate
+        Separation separate = new Separation();
+        separate.character = this;
+        GameObject[] goBirds = GameObject.FindGameObjectsWithTag("bird");
+
+        kBirds = new Kinematic[goBirds.Length - 1];
+        int j = 0;
+
+        for (int i = 0; i < goBirds.Length - 1; i++)
+        {
+            if (goBirds[i] == this)
+            {
+                continue;
+            }
+            kBirds[j++] = goBirds[i].GetComponent<Kinematic>();
+        }
+
+        separate.targets = kBirds;
+
+        // Cohere
+        Arrive cohere = new Arrive();
+        cohere.character = this;
+        cohere.target = myCohereTarget;
+
+        // Look -rotate
+        LookWhereGoing myRotateType = new LookWhereGoing();
+        myRotateType.character = this;
+
+        blendSteering = new BlendSteering();
+        blendSteering.behaviors = new BehaviorAndWeight[3];
+        blendSteering.behaviors[0] = new BehaviorAndWeight();
+        blendSteering.behaviors[0].behavior = separate;
+        blendSteering.behaviors[0].weight = 1f; 
+        blendSteering.behaviors[1] = new BehaviorAndWeight();
+        blendSteering.behaviors[1].behavior = cohere;
+        blendSteering.behaviors[1].weight = 1f; 
+        blendSteering.behaviors[2] = new BehaviorAndWeight();
+        blendSteering.behaviors[2].behavior = myRotateType;
+        blendSteering.behaviors[2].weight = 1f;
+
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        steeringUpdate = new SteeringOutput();
+        steeringUpdate = blendSteering.getSteering();
+
+        base.Update();
+    }
+}
